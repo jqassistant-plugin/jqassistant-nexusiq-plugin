8cc124abc097c4c34113edfc999de16edfa05689
Dirk Mahler <dirk.mahler@buschmais.com>
2023-08-09 19:07:34 +0200

    fixed SQ issue

--- src/main/java/org/jqassistant/plugin/nexusiq/impl/scanreport/mapper/ComponentCoordinateMapper.java
+++ src/main/java/org/jqassistant/plugin/nexusiq/impl/scanreport/mapper/ComponentCoordinateMapper.java
@@ -1,17 +1,16 @@
 package org.jqassistant.plugin.nexusiq.impl.scanreport.mapper;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.buschmais.jqassistant.core.scanner.api.Scanner;
+
 import org.jqassistant.plugin.nexusiq.api.model.scanreport.ComponentCoordinateDescriptor;
 import org.jqassistant.plugin.nexusiq.impl.mapper.DescriptorMapper;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.mapstruct.factory.Mappers.getMapper;
 
 @Mapper
 public interface ComponentCoordinateMapper extends DescriptorMapper<Map.Entry<String, String>, ComponentCoordinateDescriptor>{
